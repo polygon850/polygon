ce5cae6a7c6a7fb19940f4c7e6fb8f1dd61e6d46
polygon850 <andre.itskill@gmail.com>
2023-06-21 16:02:17 +0300

    Append zap logger

--- cmd/polygon/main.go
+++ cmd/polygon/main.go
@@ -3,29 +3,38 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"sync"
+	"time"
 
 	"github.com/jessevdk/go-flags"
 	"github.com/valyala/fasthttp"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 // main точка входа в приложение
 func main() {
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Printf("recovered from panic: %s\n", r)
-		}
-	}()
-
-	var cfg Config
-	parser := flags.NewParser(&cfg, flags.Default)
+	var cfg = new(Config)
+	parser := flags.NewParser(cfg, flags.HelpFlag|flags.PassDoubleDash)
 	_, err := parser.Parse()
 	if err != nil {
-		fmt.Printf("error on parse config: %v\n", err)
-		os.Exit(1)
+		log.Fatal(fatalJsonLog("failed to parse config", err))
 	}
+
+	logger, err := initLogger()
+	if err != nil {
+		log.Fatal(fatalJsonLog("failed to init logger", err))
+	}
+
+	defer func() {
+		if r := recover(); r != nil {
+			logger.Error("recovered from panic", zap.Error(err))
+		}
+	}()
 
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
@@ -36,7 +45,7 @@ func main() {
 	go func() {
 		defer wg.Done()
 
-		fmt.Printf("starting HTTP private server:%s\n", cfg.HttpPrivateListen)
+		logger.Info("starting HTTP private server", zap.String("address", cfg.HttpPrivateListen))
 		server := fasthttp.Server{
 			Handler: func(req *fasthttp.RequestCtx) {
 				req.SuccessString("text/html; charset=utf-8", "success...")
@@ -47,18 +56,44 @@ func main() {
 			<-ctx.Done()
 			err := server.Shutdown()
 			if err != nil {
-				fmt.Printf("error on shutdown HTTP private server: %v\n", err)
+				logger.Error("error on shutdown HTTP private server", zap.Error(err))
 			}
 		}()
 
 		err := server.ListenAndServe(cfg.HttpPrivateListen)
 		if err != nil {
-			fmt.Printf("error on listen and serve HTTP private server: %v\n", err)
+			logger.Error("error on listen and serve HTTP private server", zap.Error(err))
 		}
 
 		cancel()
 	}()
 
 	wg.Wait()
-	fmt.Println("completed...")
+	logger.Info("application completed")
+}
+
+// initLogger создает и настраивает новый экземпляр логгера
+func initLogger() (*zap.Logger, error) {
+	var cfg zap.Config
+	cfg = zap.NewProductionConfig()
+	cfg.EncoderConfig.EncodeTime = zapcore.RFC3339NanoTimeEncoder
+
+	return cfg.Build()
+}
+
+// fatalJsonLog стилизует сообщение ошибки
+func fatalJsonLog(msg string, err error) string {
+	escape := func(s string) string {
+		return strings.ReplaceAll(strings.ReplaceAll(s, `\`, `\\`), `"`, `\"`)
+	}
+	errString := ""
+	if err != nil {
+		errString = err.Error()
+	}
+	return fmt.Sprintf(
+		`{"level":"fatal","ts":"%s","msg":"%s","error":"%s"}`,
+		time.Now().Format(time.RFC3339Nano),
+		escape(msg),
+		escape(errString),
+	)
 }
--- go.mod
+++ go.mod
@@ -2,12 +2,17 @@ module github.com/polygon850/polygon
 
 go 1.20
 
-require github.com/valyala/fasthttp v1.48.0
+require (
+	github.com/jessevdk/go-flags v1.5.0
+	github.com/valyala/fasthttp v1.48.0
+	go.uber.org/zap v1.24.0
+)
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
-	github.com/jessevdk/go-flags v1.5.0 // indirect
 	github.com/klauspost/compress v1.16.6 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/sys v0.9.0 // indirect
 )
